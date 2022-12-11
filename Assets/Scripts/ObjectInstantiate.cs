a653ea524e9a268c3fb422f92f3d2fdfecdc00d7
riku0402 <04022005rikuriku@gmail.com>
2022-12-11 16:24:23 +0900

    生成完了

--- Assets/Scripts/ObjectInstantiate.cs
+++ Assets/Scripts/ObjectInstantiate.cs
@@ -1,39 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ObjectInstantiate : MonoBehaviour
 {
-    int _randomObj;
+    int _random;
+    Vector3 _mousePosition;
+    Vector3 _objPosition;
 
-    Vector3 mousePosition;
-
-    [SerializeField]
+    [SerializeField] 
     [Header("オブジェクトリスト")]
-    List<Image> _objList = new();
+    List <GameObject> _objList;
 
     [SerializeField]
-    GameObject _a;
-
-    [SerializeField]
-    [Header("Cancas")]
+    [Header("親")]
     GameObject _parent;
 
     void Update()
     {
-        Touch();
-    }
-
-    void Touch()
-    {
         if (Input.GetMouseButtonDown(0))
         {
-            _randomObj = Random.Range(0, _objList.Count);
-            mousePosition = Input.mousePosition;
-            mousePosition.z = 10.0f;
-            var prefab = Instantiate(_objList[_randomObj], Camera.main.WorldToScreenPoint(mousePosition), Quaternion.identity);
-            prefab.transform.SetParent(_parent.transform, false);
+            _random = Random.Range(0, _objList.Count);
+            _mousePosition = Input.mousePosition;
+            _mousePosition.z = 10.0f;
+            _objPosition = Camera.main.ScreenToWorldPoint(_mousePosition);
+            var prefab = Instantiate(_objList[_random], _objPosition, Quaternion.identity);
+            prefab.transform.SetParent(_parent.transform, true);
         }
     }
 }
