26b86852f571f20b6f07636bf1338ac02fb9823b
kny17s <83321238+kny17s@users.noreply.github.com>
2022-12-11 14:50:33 +0900

    オブジェクトプールを使いました

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     [SerializeField]
+    List<GameObject> _gameObjects = new();
+
+    [SerializeField]
     GameObject[] _gameObject;
 
-    Vector3 mousePosition;
+    Vector3 _mousePosition;
 
     int _num;
+
+    [SerializeField]
+    Transform _parent;
+    public void Awake()
+    {
+        ObjectPool.CreatePool(_gameObjects,_gameObject[_num],1, _parent);
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _num = Random.Range(0, _gameObject.Length);
-            mousePosition = Input.mousePosition;
-            mousePosition.z = 10.0f;
-            Instantiate(_gameObject[_num], Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
+            _num = Random.Range(0, _gameObjects.Count);
+            _mousePosition = Input.mousePosition;
+            _mousePosition.z = 10.0f;
+            //Instantiate(_gameObjects[_num], Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
+
+            ObjectPool.UseObject(_gameObjects, _gameObject[_num], _parent);
         }
     }
 }
